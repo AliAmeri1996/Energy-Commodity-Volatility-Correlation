7057dcf5eac63bfc5688e5dcef49ff6a6c838c47
Ali Ameri <aliameri82@gmail.com>
2026-07-14 19:20:02 +0100

    add analysis and visualisation

---
+++ energy_volatility.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
+import seaborn as sns
+
+
+oil=yf.download('CL=F', start='2020-01-01', end="2024-12-12")
+gas=yf.download('NG=F', start='2020-01-01', end="2024-12-12")
+gold=yf.download('GC=F', start='2020-01-01', end="2024-12-12")
+
+oil=oil.droplevel(1,axis=1)#flatten the MultiIndex on
+gas=gas.droplevel(1,axis=1)
+gold=gold.droplevel(1,axis=1)
+
+#1-Explore the data
+
+print("\n the columns of the data")
+print(oil.columns)# one is enough cause basically all got the same layout
+
+print("\n the number of rows and columns in each")
+print("\noil:",oil.shape,
+      "\ngas:",gas.shape,
+      "\ngold:",gold.shape)
+
+
+print("\n the overall information about the data")
+
+print("\noil:",oil.describe(),
+      "\ngas:",gas.describe(),
+      "\ngold:",gold.describe())
+
+
+print("\n missing vlaues in each data")
+print("\n oil:",oil.isnull().sum(),
+      "\ngold:",gold.isnull().sum(),
+      "\ngas:",gas.isnull().sum())
+
+
+#2-Cleaning the data
+
+oil=oil['Close']# keeping only the Close column
+gold=gold["Close"]
+gas=gas['Close']
+
+
+df=pd.concat([oil,gas,gold],axis=1) #axis=1 means join them side by side (columns) rather than stacking them on top of each other.
+df.columns=['Oil','Gas','Gold']
+df=df.dropna()
+
+
+#3-analysing the data
+
+df["Oil_Vol"]=df['Oil'].rolling(30).std() #calculate 30-day rolling standard deviation for each commodity
+df["Gas_Vol"]=df['Gas'].rolling(30).std()
+df["Gold_Vol"]=df['Gold'].rolling(30).std()
+
+
+correlation=df[['Oil','Gas','Gold']].corr()#.corr() calculates the correlation between every pair of columns automatically and returns a table.
+print(correlation)
+
+
+
+#4-visuliazation 
+
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(12, 14))
+
+
+#Price chart (all three commodities)
+oil['Close'].plot(ax=ax1, label='Oil')
+gas['Close'].plot(ax=ax1, label='Gas')
+gold['Close'].plot(ax=ax1, label='Gold')
+ax1.set_title('Oil vs Gold vs Gas')
+ax1.set_ylabel('Price (USD)')
+ax1.legend()
+
+
+# Chart 3 - Volatility
+df['Oil_Vol'].plot(ax=ax3, label='Oil_Volatility')
+df['Gas_Vol'].plot(ax=ax3, label='Gas_Volatility')
+df['Gold_Vol'].plot(ax=ax3, label='Gold_Volatility')
+ax3.set_title('Volatility chart')
+ax3.set_ylabel('Volatility (USD)')
+ax3.set_xlabel('Date')
+ax3.legend()
+
+
+
+sns.heatmap(correlation, annot=True, cmap='coolwarm', ax=ax4)
